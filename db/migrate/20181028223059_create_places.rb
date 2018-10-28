aa2319ec72fec82e600f406d6c2494ec2f277a54
RonSala <ron.sala@gmail.com>
2018-10-28 19:26:21 -0400

    create model attributes

--- db/migrate/20181028223059_create_places.rb
+++ db/migrate/20181028223059_create_places.rb
@@ -1,6 +1,12 @@
 class CreatePlaces < ActiveRecord::Migration
   def change
     create_table :places do |t|
+      t.string :name
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :category
+      t.string :website
 
       t.timestamps null: false
     end
--- db/migrate/20181028223125_create_reviews.rb
+++ db/migrate/20181028223125_create_reviews.rb
@@ -1,6 +1,14 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :place_id
+      t.string :title
+      t.datetime :visit
+      t.boolean :tv
+      t.integer :volume
+      t.integer :quality
+      t.text :body
 
       t.timestamps null: false
     end

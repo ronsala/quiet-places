59f70f1d19332779f0fce1367ab4e9799d0de361
RonSala <ron.sala@gmail.com>
2018-10-28 20:49:25 -0400

    add validations to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-
+  validates_presence_of :username, :email, :password_digest
+  validates_uniqueness_of :username, :email
   has_secure_password
   has_many :reviews
 end

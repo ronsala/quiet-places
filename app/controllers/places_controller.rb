d921fdd8be423ce76ad7991486efd111239273f9
RonSala <ron.sala@gmail.com>
2018-11-02 22:23:22 -0400

    code routes in app/controllers/places_controller.rb

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,37 +1,42 @@
 class PlacesController < ApplicationController
 
-  # GET: /places
   get "/places" do
+    redirect_if_not_logged_in
+    @places = Place.all
     erb :"/places/index.html"
   end
 
-  # GET: /places/new
   get "/places/new" do
+    redirect_if_not_logged_in
     erb :"/places/new.html"
   end
 
-  # POST: /places
   post "/places" do
+    redirect_if_not_logged_in
+    unless Place.valid_params?(params)
+      redirect "/places/new?error=Please include all required fields."
+    end
+    Place.create(params)
     redirect "/places"
   end
 
-  # GET: /places/5
   get "/places/:id" do
+    redirect_if_not_logged_in
     erb :"/places/show.html"
   end
 
-  # GET: /places/5/edit
   get "/places/:id/edit" do
+    redirect_if_not_logged_in
     erb :"/places/edit.html"
   end
 
-  # PATCH: /places/5
   patch "/places/:id" do
+    redirect_if_not_logged_in
     redirect "/places/:id"
   end
 
-  # DELETE: /places/5/delete
   delete "/places/:id/delete" do
+    redirect_if_not_logged_in
     redirect "/places"
   end
 end

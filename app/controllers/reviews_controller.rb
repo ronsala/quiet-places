b37391c7278b7ed31dca02170b20f3862f9a370f
RonSala <ron.sala@gmail.com>
2018-11-03 21:12:11 -0400

    code routes in app/controllers/reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,37 +1,42 @@
 class ReviewsController < ApplicationController
 
-  # GET: /reviews
-  get "/reviews" do
-    erb :"/reviews/index.html"
+  get "/places" do
+    redirect_if_not_logged_in
+    @places = Place.all
+    erb :"/places/index.html"
   end
 
-  # GET: /reviews/new
-  get "/reviews/new" do
-    erb :"/reviews/new.html"
+  get "/places/new" do
+    redirect_if_not_logged_in
+    erb :"/places/new.html"
   end
 
-  # POST: /reviews
-  post "/reviews" do
-    redirect "/reviews"
+  post "/places" do
+    redirect_if_not_logged_in
+    unless Place.valid_params?(params)
+      redirect "/places/new?error=Please include all required fields."
+    end
+    Place.create(params)
+    redirect "/places"
   end
 
-  # GET: /reviews/5
-  get "/reviews/:id" do
-    erb :"/reviews/show.html"
+  get "/places/:id" do
+    redirect_if_not_logged_in
+    erb :"/places/show.html"
   end
 
-  # GET: /reviews/5/edit
-  get "/reviews/:id/edit" do
-    erb :"/reviews/edit.html"
+  get "/places/:id/edit" do
+    redirect_if_not_logged_in
+    erb :"/places/edit.html"
   end
 
-  # PATCH: /reviews/5
-  patch "/reviews/:id" do
-    redirect "/reviews/:id"
+  patch "/places/:id" do
+    redirect_if_not_logged_in
+    redirect "/places/:id"
   end
 
-  # DELETE: /reviews/5/delete
-  delete "/reviews/:id/delete" do
-    redirect "/reviews"
+  delete "/places/:id/delete" do
+    redirect_if_not_logged_in
+    redirect "/places"
   end
 end

88e4e37f431d3602b47d8a1ec52c80e51be8f898
RonSala <ron.sala@gmail.com>
2018-12-04 05:33:27 -0500

    complete places/edit html and route

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -34,7 +34,33 @@ class PlacesController < ApplicationController
 
   get "/places/:id/edit" do
     redirect_if_not_logged_in
+    @place = Place.find(params[:id])
     erb :"/places/edit"
+  end
+
+  post "/places/:id" do
+    redirect_if_not_logged_in 
+    @place = Place.find(params[:id])
+    unless params[:name] == ""
+      @place.update(name: params[:name])
+    end
+    unless params[:street] == ""
+      @place.update(street: params[:street])
+    end
+    unless params[:city] == ""
+      @place.update(city: params[:city])
+    end
+    unless params[:state] == "--"
+      @place.update(state: params[:state])
+    end
+    unless params[:category] == ""
+      @place.update(category: params[:category])
+    end
+    unless params[:website] == ""
+      @place.update(website: params[:website])
+    end
+    # @place.update(name: params[:name], street: params[:street], city: params[:city], state: params[:state], category: params[:category], website: params[:website])
+    redirect "/places/#{@place.id}"
   end
 
   delete "/places/:id/delete" do

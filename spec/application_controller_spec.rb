686478f6e892e2b11b0bbe8bc303a08d02c64e43
RonSala <ron.sala@gmail.com>
2018-12-05 21:34:09 -0500

    code application_controller_spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -8,6 +8,6 @@ describe ApplicationController do
   it "responds with a welcome message" do
     get '/'
     expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
+    expect(last_response.body).to include("Welcome To Quiet Places!")
   end
 end

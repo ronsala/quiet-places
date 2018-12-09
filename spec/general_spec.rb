601de65c6450f8c6cccc144f78d502266829e991
RonSala <ron.sala@gmail.com>
2018-12-09 00:58:11 -0500

    pass 'signs up' test

--- spec/general_spec.rb
+++ spec/general_spec.rb
@@ -1,20 +1,14 @@
 require_relative "spec_helper"
 
 describe ApplicationController do
-  # pry(RSpec::ExampleGroups::ApplicationController_2)> app
-  # => ApplicationController
   it "signs up" do
     visit('/')
     click_link('Sign Up')
-    # save_and_open_page
-    save_screenshot('screenshot.png')
-    # pry(#<RSpec::ExampleGroups::ApplicationController_2>)> current_path
-    # => "/signup"
-    fill_in('Username * ', :with => 'Capy')
-    fill_in('Email', :with => 'capy@example.com')
-    fill_in('Password', :with => '123')
-    fill_in('Retype Password', :with => '123')
-    click_button('Submit')
+    fill_in('username', :with => 'Capy')
+    fill_in('email', :with => 'capy@example.com')
+    fill_in('password', :with => '123')
+    fill_in('password_confirm', :with => '123')
+    click_button('submit')
     expect(page).to have_content('Logged in as Capy')
   end
 

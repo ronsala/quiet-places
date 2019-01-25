5bbc1df2c4a904163c1a304b325393d732b79553
RonSala <ron.sala@gmail.com>
2019-01-25 02:14:04 -0500

    inplement Howard's suggestions for #current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,9 +22,7 @@ class ApplicationController < Sinatra::Base
 
     def current_user
       if logged_in?
-        User.find(session[:user_id])
-      else
-        erb :index
+        User.find_by(id: session[:user_id])
       end
     end
 

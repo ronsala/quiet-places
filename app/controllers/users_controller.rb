28c12feadac99e6d31c31943286e6dfd52d77153
RonSala <ron.sala@gmail.com>
2019-02-12 03:00:52 -0500

    sort place names in users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   get "/users/:id" do
     @user = User.find(params[:id])
-    @places = @user.places
+    @places = @user.places.sort_by { |place | place.name }
     @reviews = @user.reviews
     erb :"/users/show"
   end

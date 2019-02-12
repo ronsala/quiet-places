306c1901d76d1f83bbeb9be95aa6f63e0072b484
RonSala <ron.sala@gmail.com>
2019-02-11 22:47:53 -0500

    add 'set :show_exceptions, false' to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions unless test?
+    set :show_exceptions, false
     register Sinatra::Flash
     set :session_secret, ENV["SECRET_KEY"]
   end

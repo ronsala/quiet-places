634827ee175c4ad9fed7d6c70925fcb214a7b946
RonSala <ron.sala@gmail.com>
2019-02-22 05:07:47 -0500

    finish improving validation for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,19 +14,16 @@ class PlacesController < ApplicationController
     redirect_if_not_logged_in
     @place = Place.new(name: params[:name], street: params[:street], city: params[:city], state: params[:state], category: params[:category], website: params[:website])
 
-    if @place.invalid? || @place.state == "--"
-      if @place.errors.any? 
-        if @place.errors.messages[:name]
-          flash[:name] = "Name #{@place.errors.messages[:name][0]}. Please try again."
-        elsif @place.errors.messages[:street]
-          flash[:street] = "Street #{@place.errors.messages[:street][0]}. Please try again."
-        elsif @place.errors.messages[:city]
-          flash[:city] = "City #{@place.errors.messages[:city][0]}. Please try again."
-        end
-      end
-        
-      if @place.state == "--"
-        flash[:state] = "You must select a state."
+    if @place.invalid?
+      
+      if @place.errors.messages[:name]
+        flash[:name] = "Name #{@place.errors.messages[:name][0]}. Please try again."
+      elsif @place.errors.messages[:street]
+        flash[:street] = "Street #{@place.errors.messages[:street][0]}. Please try again."
+      elsif @place.errors.messages[:city]
+        flash[:city] = "City #{@place.errors.messages[:city][0]}. Please try again."
+      elsif @place.errors.messages[:state]
+        flash[:state] = "State #{@place.errors.messages[:state][0]}. Please try again."
       end
 
       redirect '/places/new'  
@@ -56,30 +53,26 @@ class PlacesController < ApplicationController
 
   patch "/places/:id" do
     @place = Place.find(params[:id])
+
     unless current_user == @place.user || current_user.is_admin
       redirect "/"
     end
+
     @place.errors.clear
-    unless params[:name] == ""
-      @place.update(name: params[:name])
-    end
-    unless params[:street] == ""
-      @place.update(street: params[:street])
-    end
-    unless params[:city] == ""
-      @place.update(city: params[:city])
-    end
-    unless params[:state] == "--"
-      @place.update(state: params[:state])
-    end
-    unless params[:category] == ""
-      @place.update(category: params[:category])
-    end
-    unless params[:website] == ""
-      @place.update(website: params[:website])
-    end
-    if @place.errors.messages != {}
-      flash[:name] = "#{@place.errors.full_messages[0]}. Please try again."
+
+    @place.update(name: params[:name], street: params[:street], city: params[:city], state: params[:state], category: params[:category], website: params[:website])
+
+    if @place.errors.any? 
+      if @place.errors.messages[:name]
+        flash[:name] = "Name #{@place.errors.messages[:name][0]}. Please try again."
+      elsif @place.errors.messages[:street]
+        flash[:street] = "Street #{@place.errors.messages[:street][0]}. Please try again."
+      elsif @place.errors.messages[:city]
+        flash[:city] = "City #{@place.errors.messages[:city][0]}. Please try again."
+      elsif @place.errors.messages[:state]
+        flash[:state] = "State #{@place.errors.messages[:state][0]}. Please try again."
+      end
+
       redirect "/places/#{@place.id}/edit"
     else
       redirect "/places/#{@place.id}"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,5 @@
 class Place < ActiveRecord::Base
-  validates :name, :street, :city, presence: true
+  validates :name, :street, :city, :state, presence: true
   validates :name, uniqueness: true
   belongs_to :user
   has_many :reviews

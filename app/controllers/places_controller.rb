a1c51d65431f1c00db98f659bd1f52eb14b7b284
RonSala <ron.sala@gmail.com>
2019-02-22 04:20:31 -0500

    improve validation process for places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,16 +12,24 @@ class PlacesController < ApplicationController
 
   post "/places" do
     redirect_if_not_logged_in
-    @place = Place.create(name: params[:name], street: params[:street], city: params[:city], state: params[:state], category: params[:category], website: params[:website])
-    if @place.errors.any?
-      if @place.errors.messages[:name]
-        flash[:name] = "Name #{@place.errors.messages[:name][0]}. Please try again."
-      elsif @place.errors.messages[:street]
-        flash[:street] = "Street #{@place.errors.messages[:street][0]}. Please try again."
-      elsif @place.errors.messages[:city]
-        flash[:city] = "City #{@place.errors.messages[:city][0]}. Please try again."
+    @place = Place.new(name: params[:name], street: params[:street], city: params[:city], state: params[:state], category: params[:category], website: params[:website])
+
+    if @place.invalid? || @place.state == "--"
+      if @place.errors.any? 
+        if @place.errors.messages[:name]
+          flash[:name] = "Name #{@place.errors.messages[:name][0]}. Please try again."
+        elsif @place.errors.messages[:street]
+          flash[:street] = "Street #{@place.errors.messages[:street][0]}. Please try again."
+        elsif @place.errors.messages[:city]
+          flash[:city] = "City #{@place.errors.messages[:city][0]}. Please try again."
+        end
       end
-      redirect '/places/new'
+        
+      if @place.state == "--"
+        flash[:state] = "You must select a state."
+      end
+
+      redirect '/places/new'  
     else
       @place.user_id = current_user.id
       @user = User.find(@place.user_id)
